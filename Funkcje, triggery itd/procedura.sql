9bfcb5e41574f7ea65a7d863c6dd1d3a08ff77d0
arekkowalski <73654010+arekkowalski@users.noreply.github.com>
2021-01-06 21:54:21 +0100

    Delete procedura.sql

--- Funkcje, triggery itd/procedura.sql
+++
@@ -1,8 +0,0 @@
-DELIMITER $$
-USE `arekkowalski`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `miesiac_suma_zamowien`(IN liczba int, OUT suma int)
-BEGIN
-SELECT  count(*) INTO suma from  zamowienie   where month(data_zamowienia) = liczba;
-END$$
-
-DELIMITER ;
